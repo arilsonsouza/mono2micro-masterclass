46cbe0c409e1adc62253547f07febbdf845c0d43
Arilson Souza <arilsons61@gmail.com>
2024-06-17 12:03:40 -0300

    feat: add flight domain

---
+++ mono/travel-app/src/main/java/org/aos/flight/Flight.java
@@ -0,0 +1,14 @@
+package org.aos.flight;
+
+import io.quarkus.hibernate.orm.panache.PanacheEntity;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+
+@Entity(name = "flight")
+@Table(name = "flights")
+public class Flight extends PanacheEntity {
+
+  public Long orderId;
+  public String fromAirport;
+  public String toAirport;
+}
